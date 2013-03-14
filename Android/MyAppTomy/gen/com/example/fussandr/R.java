4d12ba2dc2c669020eb3fec388587dda06ab4282
Tomislav Slade <tomislav.slade@gmail.com>
2013-03-14 23:34:11 +0100

    connection change receiver-x

--- Android/MyAppTomy/gen/com/example/fussandr/R.java
+++ Android/MyAppTomy/gen/com/example/fussandr/R.java
@@ -27,26 +27,27 @@ public final class R {
         public static final int video_camera=0x7f020007;
     }
     public static final class id {
-        public static final int buttonNew=0x7f090009;
-        public static final int buttonNewPicture=0x7f09000a;
-        public static final int buttonNewVideo=0x7f09000b;
-        public static final int buttonUpdate=0x7f090008;
-        public static final int editText=0x7f090007;
-        public static final int imageView1=0x7f090004;
-        public static final int itemPrefs=0x7f090012;
-        public static final int itemToggleService=0x7f090013;
-        public static final int listTimeline=0x7f09000c;
-        public static final int menu_settings=0x7f09000f;
-        public static final int settingsButton=0x7f09000d;
-        public static final int start=0x7f090010;
-        public static final int stop=0x7f090011;
-        public static final int textCount=0x7f090006;
+        public static final int buttonNew=0x7f09000a;
+        public static final int buttonNewPicture=0x7f09000b;
+        public static final int buttonNewVideo=0x7f09000c;
+        public static final int buttonUpdate=0x7f090009;
+        public static final int editText=0x7f090008;
+        public static final int imageView1=0x7f090005;
+        public static final int itemPrefs=0x7f090013;
+        public static final int itemToggleService=0x7f090014;
+        public static final int itemsLayout=0x7f090004;
+        public static final int listTimeline=0x7f09000d;
+        public static final int menu_settings=0x7f090010;
+        public static final int settingsButton=0x7f09000e;
+        public static final int start=0x7f090011;
+        public static final int stop=0x7f090012;
+        public static final int textCount=0x7f090007;
         public static final int textCreatedAt=0x7f090001;
         public static final int textText=0x7f090002;
         public static final int textTimeline=0x7f090003;
         public static final int textUser=0x7f090000;
-        public static final int videoView1=0x7f090005;
-        public static final int yamba_icon=0x7f09000e;
+        public static final int videoView1=0x7f090006;
+        public static final int yamba_icon=0x7f09000f;
     }
     public static final class layout {
         public static final int row=0x7f030000;
